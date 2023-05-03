213334b5dcced3907c4370481dbcb8cc01ed5807
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-03 23:24:30 +0900

    add header component

---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar density="compact" color="#db4c3f">
+    <v-app-bar-nav-icon color="#fff"></v-app-bar-nav-icon>
+    <v-btn icon>
+      <v-icon color="#fff">mdi-home-outline</v-icon>
+    </v-btn>
+    <v-responsive class="mx-auto" max-width="300">
+      <v-text-field
+        prepend-inner-icon="mdi-magnify"
+        color="#fff"
+        base-color="#fff"
+        bg-color="#eda59e"
+        density="compact"
+        variant="solo"
+        hide-details
+        single-line
+        clearable
+      >
+      </v-text-field>
+    </v-responsive>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon color="#fff">mdi-plus</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon color="#fff">mdi-trending-up</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon color="#fff">mdi-help-circle-outline</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon color="#fff">mdi-bell-outline</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-avatar color="surface-variant"></v-avatar>
+    </v-btn>
+  </v-toolbar>
+</template>
