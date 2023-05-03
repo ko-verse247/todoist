fbc8997f4c4d35fe9c21407849e5ac27ca723696
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-03 23:24:05 +0900

    start using vuetify + mdi

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+    components,
+    directives,
+  })
+
+createApp(App).use(vuetify).mount('#app')
