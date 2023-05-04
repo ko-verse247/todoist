959cf856e24959a7a3e2568ee59320c699b04321
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-04 17:19:12 +0900

    update PageWrapper to be responsive

---
+++ vue-project/src/components/PageWrapper.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  computed: {
+    isMobile() {
+      return window.innerWidth <= 600;
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-if="isMobile">
+    <v-col>
+      <v-sheet class="pa-2 ma-2">
+        <slot></slot>
+      </v-sheet>
+    </v-col>
+  </div>
+  <div v-else>
+    <v-row no-gutters>
+      <v-col>
+        <v-sheet class="pa-2 ma-2"></v-sheet>
+      </v-col>
+      <v-col cols="6">
+        <v-sheet class="pa-2 ma-2">
+          <slot></slot>
+        </v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-2 ma-2"></v-sheet>
+      </v-col>
+    </v-row>
+  </div>
+</template>
