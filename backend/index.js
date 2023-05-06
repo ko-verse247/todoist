84906183f9f5f59fe581fd813474ba513ed67564
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-06 21:28:54 +0900

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -16,6 +17,7 @@ mongoose
     console.log("CONNECTED TO DATABASE");
   });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", todoRoutes);
 
