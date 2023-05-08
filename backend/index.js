b1cc1901e75f7b963fa32d472c5c7192f895f260
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-08 16:53:46 +0900

    add process.env.PORT

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const todoRoutes = require("./routes/Todo");
 
