ae1a4dba13e40980c1214408ca4e3211944b728c
LuizFernandoVieiraFerreira <luizfernandovieiraferreira@gmail.com>
2023-05-08 18:07:08 +0900

    update Date component to actually get the current date instead of hard coded date

--- frontend/src/components/Date.vue
+++ frontend/src/components/Date.vue
@@ -1,4 +1,18 @@
+<script>
+import dayjs from 'dayjs'
+import 'dayjs/locale/ko'
+
+export default {
+  name: 'Date',
+  data() {
+    return {
+      date: dayjs().locale('ko').format('ddd MMMM D일'),
+    };
+  },
+}
+</script>
+
 <template>
   <span class="text-h5 font-weight-bold">오늘 </span>
-  <span class="font-weight-thin">목 4원 21일</span>
+  <span class="font-weight-thin">{{date}}</span>
 </template>
