8596828d513580367911bc3b664ade3055b4cdcf
Caden <cpepper1@asu.edu>
2023-06-15 22:55:10 -0700

    server update

--- todoist-caden-backend/server.js
+++ todoist-caden-backend/server.js
@@ -13,16 +13,22 @@ const app = express();
 const PORT = process.env.PORT;
 var uri = process.env.DB_URI;
 
-const mydb = mongoose.connect(uri, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+const mydb = mongoose.connect(uri, {}, (err, res) => {
+  try {
+    console.log("connected to database");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://todoist-caden.onrender.com");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://todoist-caden.onrender.com"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
