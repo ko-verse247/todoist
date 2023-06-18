da20d2c0897a67bb7384bd697a1350198e44051b
Batman <32883202+CynthiaLomas@users.noreply.github.com>
2023-06-19 08:37:26 +0900

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Task from './components/Task'
+
+export default {
+  name: 'App',
+  components: {
+    Task
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:5000/tasks')
+      .then((response) => {
+        console.log(response.data)
+        this.tasks = response.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+</script>
---
+++ frontend/src/components/TaskItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <button @click.self="$emit('complete')" :class="className"><i class="far fa-circle"></i> {{task.task}}</button>
+    <!-- <button @click="$emit('edit')"><i class="far fa-pen-to-square"></i></button> -->
+    <button @click="$emit('remove')"><i class="far fa-trash-alt"></i></button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: ['task'],
+  computed: {
+    className () {
+      let classes = ['toggle']
+      if (this.task.completed) {
+        classes.push('toggle-completed')
+      }
+      return classes.join(' ')
+    }
+  }
+}
+</script>
